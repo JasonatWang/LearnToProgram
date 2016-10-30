a7538d5ab59f132c6343230dbb6cf61e36be9702
jason_wangxiao@163.com <jason_wangxiao@163.com>
2016-10-31 00:37:17 +0800

    新增turtle画出数字4

---
+++ 2016_10_Python/learn_turtle.py
@@ -0,0 +1,9 @@
+import turtle
+def turtlefd(dis,degree):
+    turtle.left(degree)
+    turtle.forward(dis)
+turtle.pensize(20)
+turtle.pencolor("lightgreen")
+turtlefd(100,90)
+turtlefd(96.8,135)
+turtlefd(100,135)
