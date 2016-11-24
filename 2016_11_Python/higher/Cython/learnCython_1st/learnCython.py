44b931d23ffd795461449db90e4ec45c0d9f11e4
jason_wangxiao@163.com <jason_wangxiao@163.com>
2016-11-24 14:58:53 +0800

    成功做成了Cython

---
+++ 2016_11_Python/higher/Cython/learnCython_1st/learnCython.py
@@ -0,0 +1,3 @@
+import pythonCython
+res_sum = pythonCython.say_hello(1, 2)
+print('the result of cython is: ', res_sum)
---
+++ 2016_11_Python/higher/Cython/learnCython_1st/pythonCython.pyx
@@ -0,0 +1,3 @@
+def say_hello(int i, int j):
+    print("hello", i, "and", j)
+    return i + j
